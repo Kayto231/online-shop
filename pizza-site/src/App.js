cf6cb16d1151a1aa0a8793c1fc35ccf3c3ec5453
Alex <oleksiym81@icloud.com>
2022-04-16 21:07:33 +0300

    Some small UI

--- pizza-site/src/App.js
+++ pizza-site/src/App.js
@@ -9,34 +9,31 @@ import Banner from "./components/Banner/Banner.jsx";
 import {
   getCartItems,
   getFavoriteItems,
-  getSneakersBackEnd
+  getSneakersBackEnd,
 } from "./redux/actions/userActions.js";
 import { useDispatch, useSelector } from "react-redux";
 
-// import { initializeApp } from "firebase/app";
-
 function App() {
   const dispatch = useDispatch();
-  const { isCartOpened, isBannerOpened } = useSelector(state => state.sneakers);
-
-  // loading and unacking API with items for the first load
-  useEffect(
-    () => {
-      dispatch(getSneakersBackEnd());
-      dispatch(getCartItems());
-      dispatch(getFavoriteItems());
-    },
-    [dispatch]
+  const { isCartOpened, isBannerOpened } = useSelector(
+    (state) => state.sneakers
   );
 
+  // loading and unpacking API with items for the first load
+  useEffect(() => {
+    dispatch(getSneakersBackEnd());
+    dispatch(getCartItems());
+    dispatch(getFavoriteItems());
+  }, [dispatch]);
+
   return (
-    <div className="wrapper clear">
+    <div className='wrapper clear'>
       {isCartOpened && <Drawer />}
       <Header />
       {isBannerOpened && <Banner />}
       <Switch>
-        <Route exact path="/" component={ActionsCard} />
-        <Route exact path="/favorite" component={Favorite} />
+        <Route exact path='/' component={ActionsCard} />
+        <Route exact path='/favorite' component={Favorite} />
       </Switch>
     </div>
   );
